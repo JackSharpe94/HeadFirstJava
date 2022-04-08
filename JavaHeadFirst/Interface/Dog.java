d42a71595d1ddd09433bab17e32361ef9ddb9f99
Jackilton94 <81485911+Jackilton94@users.noreply.github.com>
2022-04-08 19:16:55 +0100

    Added interface examples

---
+++ JavaHeadFirst/Interface/Dog.java
@@ -0,0 +1,16 @@
+
+public class Dog extends Canine implements Pet {
+	
+	public void beFriendly() {
+		
+	}
+	
+	public void play() {
+		
+	}
+	
+	//These above must be implemented as the interface Pet contains the abstract method. Dog is a concrete
+	//method therefore must provide definition
+	
+	//Multiple interfaces can be declared
+}
